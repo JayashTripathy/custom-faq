8d6d3cdeef96cdd704809d8828da368d0133d3ea
JayashTripathy <jayashtripathy371@gmail.com>
2023-11-29 14:00:03 +0530

    fixed getTheme error

--- src/utils/getPageTheme.tsx
+++ src/utils/getPageTheme.tsx
@@ -1,6 +1,6 @@
 import { pagethemes } from "./pageThemes";
 
-export const getTheme = (themeName: string ) => {
+export const getTheme = (themeName?: string ) => {
   const theme = pagethemes.find((theme) => themeName === theme.name)?.styles;
 
   return theme;
